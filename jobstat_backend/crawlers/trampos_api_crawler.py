8010edbd6f6a4c1c2f7d45b4aea0b49aa1e2d4aa
ryanOM <omullan.ryan@gmail.com>
2016-12-01 10:24:46 -0200

    Add counter limit to trampos_api_crawler

--- jobstat_backend/crawlers/trampos_api_crawler.py
+++ jobstat_backend/crawlers/trampos_api_crawler.py
@@ -8,13 +8,16 @@ JOB_PLATFORM = "trampos"
 API_URL = 'http://trampos.co/api/v2/opportunities/%s'
 SAVE_PATH = '../crawled_data/%s' % JOB_PLATFORM
 
+# Quits after encountering X offers that aren't available or already saved
+ERROR_COUNTER = 200
 
-def get_api_data(job_id):
-    response = urllib2.urlopen(API_URL % job_id)
+
+def get_api_data(url):
+    response = urllib2.urlopen(url)
     return json.load(response)
 
 
-def save_data(data_json, job_id):
+def save_data(data_json, job_id, error_counter):
     file_name = "%s-%s.json" % (JOB_PLATFORM, job_id)
     save_path = '../crawled_data/trampos_json/%s' % file_name
 
@@ -24,14 +27,18 @@ def save_data(data_json, job_id):
             print'Created file: %s' % file_name
     else:
         print'File already exists: %s' % file_name
+        error_counter -= 1
+
+    return error_counter
 
 
-def crawl_api(job_id):
-    data = get_api_data(job_id)
+def crawl_api(api_url, job_id, error_counter):
+    data = get_api_data(api_url % job_id)
     if 'status' in data:
-        print("%s: %s" %(job_id, data['status']))
+        print("%s: %s" % (job_id, data['status']))
+        return error_counter - 1
     else:
-        save_data(data, job_id)
+        return save_data(data, job_id, error_counter)
 
 
 def check_or_create_save_folder(folder_path):
@@ -42,16 +49,27 @@ def check_or_create_save_folder(folder_path):
 
 def get_latest_job_id():
     data = get_api_data('http://trampos.co/api/v2/opportunities')
-    bob = 42
+    highest_id = 0
+
+    for opportunity in data['opportunities']:
+        if opportunity['id'] > highest_id:
+            highest_id = opportunity['id']
+
+    print "Start Crawling from jobId: %s" % highest_id
+    return highest_id
 
 
 def main():
-    get_latest_job_id()
-    start_id = START_ID
     check_or_create_save_folder(SAVE_PATH)
-    while start_id > 0:
-        crawl_api(start_id)
-        start_id -= 1
+    job_id = get_latest_job_id()
+
+    error_counter = ERROR_COUNTER
+    while job_id > 0 and error_counter > 0:
+        error_counter = crawl_api(API_URL, job_id, error_counter)
+        job_id -= 1
+
+        if error_counter <= 0:
+            print "\nNo new jobs found. Exiting..."
 
 if __name__ == '__main__':
     main()
